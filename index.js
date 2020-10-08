839cb1983d5dd2a433aba35f7e6ef33c687d34ec
Nick Watson <ngwatson79@yahoo.com>
2020-10-08 12:27:20 -0400

    Task 5

--- index.js
+++ index.js
@@ -208,7 +208,26 @@ class Instructor extends Lambdasian {
         + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
-class Student {}
+class Student extends Lambdasian {
+  constructor(attr3) {
+    super(attr3);
+    this.previousBackground = attr3.previousBackground;
+    this.className = attr3.className;
+    this.favSubjects = attr3.favSubjects;
+  }
+
+  listSubjects() {
+    return `Loving ${this.favSubjects.join()}!`;
+  }
+
+  PRAssignment(subject) {
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
+  }
+}
 
 /*
   TASK 6
